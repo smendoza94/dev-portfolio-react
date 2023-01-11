91905b54c8af36684a2d274bef28c61141b9f660
StevenM <stevenbmendoza@gmail.com>
2023-01-10 21:06:21 -0800

    start portfolio component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import About from "./components/About";
 import Nav from "./components/Nav";
+import Portfolio from "./components/Portfolio";
 
 function App() {
   return (
     <div>
       <Nav></Nav>
       <main>
+        <Portfolio></Portfolio>
         <About></About>
       </main>
     </div>
